b765c1f8dc70ccfcfb8a7965da775bcd77d51a11
Tushar <tusharbehera32@gmail.com>
2022-09-04 05:56:19 +0530

    For loop Applied

--- Project/Ticket_booking/book_my_ticket.go
+++ Project/Ticket_booking/book_my_ticket.go
@@ -9,55 +9,41 @@ func main() {
 	appName := "Book My Ticket 🙂"
 	const totalTicket int = 50
 	var remainingTicket uint = 50
-	// var bookings [50]string				// Array declaration
-	bookings := []string{}					// Slice Initialization dynamic style so no need to enter size
-
+	bookings := []string{}
 	fmt.Printf("\nWelcome to %v\n", appName)
 	fmt.Printf("Todays Ticket count is : %v and Remaining : %v Tickets are available\n", totalTicket, remainingTicket)
 	fmt.Println("Book your ticket 🎫 now to enjoy your Shows 🤓 ")
 
-	// INPUT FROM USER
+	// Use of For loop
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
 
-	fmt.Println("\n\nPlease Enter your First Name : ")
-	fmt.Scan(&firstName)
+		fmt.Printf("\n\nPlease Enter your First Name :\n")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please Enter your Last Name : ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please Enter your Last Name :")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please Enter your Email : ")
-	fmt.Scan(&email)
+		fmt.Println("Please Enter your Email :")
+		fmt.Scan(&email)
 
-	fmt.Println("No of Tickets to book :")
-	fmt.Scan(&userTicket)
+		fmt.Println("No of Tickets to book :")
+		fmt.Scan(&userTicket)
 
-	remainingTicket = remainingTicket - userTicket
-	
+		remainingTicket = remainingTicket - userTicket
+		bookings = append(bookings, firstName+" "+lastName)
 
-	// bookings[0] = firstName + " " + lastName        -->   // Array initialization
+		fmt.Printf("\n\nHello Dear [ %v %v ] Thank You For Booking [ %v ] Tickets 🤩", firstName, lastName, userTicket)
 
+		fmt.Printf("\nYour Confirmation will be sent to [ %v ] this email. Enjoy👍\n", email)
 
-	// fmt.Printf("\n\nThe whole Array : %v\n",bookings)
-	// fmt.Printf("First value of Array : %v\n",bookings[0])
-	// fmt.Printf("Type of Array : %T\n",bookings)
-	// fmt.Printf("Length of the Array : %v\n",len(bookings))
+		fmt.Printf("\nHere are all the Booking Lists : %v %v 🎫\n", bookings, userTicket)
 
+		fmt.Printf("\nReamining %v Tickets Available ...Hurry Now ✨\n", remainingTicket)
+	}
 
-	// USE OF SLICE ----->
-
-	bookings = append(bookings, firstName + " " + lastName)
-
-	// fmt.Printf("\n\nThe whole Slice : %v\n",bookings)
-	// fmt.Printf("First value of Slice : %v\n",bookings[0])
-	// fmt.Printf("Type of Slice : %T\n",bookings)
-	// fmt.Printf("Length of the Slice : %v\n",len(bookings))
-
-	fmt.Printf("\n\nHello Dear %v %v Thank You For Booking %v Tickets 🤩", firstName, lastName, userTicket)
-	fmt.Printf("\nYour Conformation will be sended on %v this email . Enjoy 👍\n", email)
-
-	fmt.Printf("Reamining %v Tickets Available ...Hurry Now ✨", remainingTicket)
 }
